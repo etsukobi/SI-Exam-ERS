9ce8c73db54c4be83234e3a9ea746fb61268c378
kittel <122339329+etsukobi@users.noreply.github.com>
2024-02-27 22:42:06 +0800

    Delete src directory

--- src/src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { CompanyReducer } from "./Reducer";
-import { thunk } from "redux-thunk";
-import { logger } from "redux-logger";
-
-const rootreducer=combineReducers({company:CompanyReducer})
-const compstore=configureStore({reducer:rootreducer,applymiddleware:[thunk,logger]})
-export default compstore;
